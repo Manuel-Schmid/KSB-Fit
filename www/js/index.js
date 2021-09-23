128df492f46dbe2d72edd9ac5179bd211eceaf9d
Manuel Schmid <manysch3@gmail.com>
2021-09-23 20:48:29 +0200

    added qr-scanner-backend

--- www/js/index.js
+++ www/js/index.js
@@ -458,3 +458,34 @@ function switchLogin(tab) {
         document.getElementById('login-btn').innerHTML = 'Speichern'
     }
 }
+
+
+// QR-Scanner
+
+function qrscan() {
+    alert("Es wird jetzt der QR Scanner aktiviert");
+    // Start a scan. Scanning will continue until something is detected or
+    // `QRScanner.cancelScan()` is called.
+    QRScanner.scan(displayContents);
+    // Make the webview transparent so the video preview is visible behind it.
+    QRScanner.show(function(status) {
+        console.log(status);
+    });
+    // Be sure to make any opaque HTML elements transparent here to avoid
+    // covering the video.
+    QRScanner.cancelScan(function(status) {
+        console.log(status)
+    });
+    // QRScanner.hide(function(status) {
+    //     console.log(status);
+    // });
+}
+
+function displayContents(err, text) {
+    if (err) {
+        // an error occurred, or the scan was canceled (error code `6`)
+    } else {
+        // The scan completed, display the contents of the QR code:
+        alert(text);
+    }
+}
