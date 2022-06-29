c21e0a2a0343bc2e6dd0cea83fbb0539a60e86b9
Manuel Schmid <manysch3@gmail.com>
2022-06-29 15:56:28 +0200

    added dynamic gallery and list-view for exercises

--- platforms/android/app/src/main/assets/www/js/index.js
+++ platforms/android/app/src/main/assets/www/js/index.js
@@ -386,7 +386,7 @@ function switchToTab(tabName) {
 //     }
 // }
 
-function displayExercises() {
+function displayExercises(galleryView) {
     // load exercises as HTML popups from DB
     $.ajax({
         type:"POST",  //Request type
@@ -399,14 +399,22 @@ function displayExercises() {
             exercises.forEach(exercise => {
 
                 functionString = "openExercisePopup(false, '"+exercise['title']+"', '"+exercise['image']+"', '"+exercise['preparation']+"', '"+exercise['movement']+"', '"+exercise['muscleGroups']+"', '"+exercise['videoURL']+"')";
-                exercisesHTMLString += '<div class="box" onclick="'+functionString+'">'+
-                                        '<div class="row">'+
-                                            '<p class="col s11">'+exercise['title']+'</p>'+
-                                            '<svg class="col s1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill-rule="evenodd" d="M13.22 19.03a.75.75 0 001.06 0l6.25-6.25a.75.75 0 000-1.06l-6.25-6.25a.75.75 0 10-1.06 1.06l4.97 4.97H3.75a.75.75 0 000 1.5h14.44l-4.97 4.97a.75.75 0 000 1.06z"></path></svg>'+
-                                        '</div>'+
-                                        '<img src="'+exercise['image']+'" alt="Übung Bild">'+
-                                    '</div>';
-
+                if (galleryView) {
+                    exercisesHTMLString += '<div class="box" onclick="'+functionString+'">'+
+                                                '<div class="row">'+
+                                                    '<p class="col s11">'+exercise['title']+'</p>'+
+                                                    '<svg class="col s1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill-rule="evenodd" d="M13.22 19.03a.75.75 0 001.06 0l6.25-6.25a.75.75 0 000-1.06l-6.25-6.25a.75.75 0 10-1.06 1.06l4.97 4.97H3.75a.75.75 0 000 1.5h14.44l-4.97 4.97a.75.75 0 000 1.06z"></path></svg>'+
+                                                '</div>'+
+                                                '<img src="'+exercise['image']+'" alt="Übung Bild">'+
+                                            '</div>';
+                } else {
+                    exercisesHTMLString += '<div class="box" onclick="'+functionString+'">'+
+                                                '<div class="row">'+
+                                                    '<span class="col s11">'+exercise['title']+'</span>'+
+                                                    '<svg class="col s1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill-rule="evenodd" d="M13.22 19.03a.75.75 0 001.06 0l6.25-6.25a.75.75 0 000-1.06l-6.25-6.25a.75.75 0 10-1.06 1.06l4.97 4.97H3.75a.75.75 0 000 1.5h14.44l-4.97 4.97a.75.75 0 000 1.06z"></path></svg>'+
+                                                '</div>'+
+                                            '</div>';
+                }
             });
 
             document.getElementById('exercises-list').innerHTML = exercisesHTMLString
@@ -499,7 +507,7 @@ function goToExercises() {
 
     document.getElementById('exercises').className = ""
     switchToTab('exercises')
-    displayExercises();
+    displayExercises(true);
 }
 
 function goToHome() {
